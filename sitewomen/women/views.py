039e574b2bc4f3bb0255fee45ba53b9914b70e76
Alex_Bor_Work <enenise@mail.ru>
2025-09-28 22:06:19 +0300

    step 4.5

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.http import HttpResponse, HttpResponseNotFound
-from women.models import Women
+from women.models import Women, Category
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
@@ -14,12 +14,6 @@ data_db = [
      'is_published': True},
     {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
     {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
-]
-
-cats_db = [
-    {'id': 1, 'name': 'Актрисы'},
-    {'id': 2, 'name': 'Певицы'},
-    {'id': 3, 'name': 'Спортсменки'},
 ]
 
 def index(request):
@@ -40,10 +34,6 @@ def about(request):
         'menu': menu,
     }
     return render(request, 'women/about.html', context=data)
-
-
-def page_not_found(request, exception):
-    return HttpResponseNotFound(f'<h1>Страница не не найдена</h1>')
 
 
 def addpage(request):
@@ -70,14 +60,19 @@ def show_post(request, post_slug):
 
     return render(request, 'women/post.html', data)
 
-def show_category(request, cat_id):
+def show_category(request, cat_slug):
+    category = get_object_or_404(Category, slug=cat_slug)
+    post = Women.objects.filter(cat_id=category.pk)
     data = {
-        'title': 'Главная страница',
+        'title': f'Рубрика {category.name}',
         'menu': menu,
-        'posts': data_db,
-        'cat_selected': cat_id
+        'posts': post,
+        'cat_selected': category.pk
     }
     return render(request, 'women/index.html', context=data)
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound(f'<h1>Страница не не найдена</h1>')
 
 
 
